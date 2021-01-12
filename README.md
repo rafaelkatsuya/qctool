9fe35693e405371e42bad9fbe1d330bd083b6f7a
Rafael Pacheco <rafael.katsuya@gmail.com>
2021-01-12 12:26:30 +0900

    First version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# qctool
+# QC Tool
+
+QC Tool provides QC checks for Japanese translations.
+
+## How to use
+
+Open `index.html`, insert your translation and click on `QC Check`.
---
+++ qc.js
@@ -0,0 +1,49 @@
+const forbiddenCharacters = ['！','？','：','（','）','　','･']
+
+function addForm() {
+  let newFormDiv = document.createElement('div');
+  let newOutputDiv = document.createElement('div'); 
+  $(newFormDiv).html('<textarea rows = "5" class="text-input"></textarea>');
+  $(newOutputDiv).addClass('text-output');
+  $(newFormDiv).append(newOutputDiv);
+  $(newFormDiv).addClass('check-form');
+  document.body.append(newFormDiv);
+}
+
+function removeForm() {
+  $('.check-form :last').remove();
+}
+
+function jaCharacterCount(string) {
+    let halfWidthCount = (string.match(/[a-zA-Z0-9]/g) || []).length / 2;
+    return string.length - halfWidthCount;
+}
+
+$('#button-add').on('click', function(evt) {
+  addForm();
+});
+
+$('#button-remove').on('click', function(evt) {
+  removeForm();
+});
+
+$('#button-check').on('click', function(evt) {
+  $('.check-form').each(function(){
+    let inputText = $(this).find('.text-input').val();
+    if (inputText) {
+      let textOutput = $(this).find('.text-output')
+      textOutput.html('Character count: ' + jaCharacterCount(inputText) + '<br>');
+
+      for (var i = 0; i < inputText.length; i++) {
+        if (forbiddenCharacters.includes(inputText[i])) {
+          textOutput.html(textOutput.html() + '<span class="forbidden">' + inputText[i]) + '</span>';
+        } else if (/\s/.test(inputText[i])) {
+          textOutput.html(textOutput.html() + '<span class="space">' + inputText[i]) + '</span>';
+        } else {
+          textOutput.html(textOutput.html() + inputText[i]);
+        }
+      }
+    }
+  });
+  return false;
+});
